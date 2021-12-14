05b5fc4e1a3cb11b165adc0dad09f932ab26f358
Cristina Oechsle <crisoechsle@gmail.com>
2021-12-14 15:30:55 +0100

    city search project

--- grocery-list/app.js
+++ grocery-list/app.js
@@ -1,5 +1,9 @@
 // ****** SELECT ITEMS **********
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
 
+const grocery = document.querySelector(".grocery");
+const submitBtn = document.querySelector(".submit-btn");
 // edit option
 
 // ****** EVENT LISTENERS **********
